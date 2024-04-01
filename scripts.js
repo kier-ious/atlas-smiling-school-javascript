0cf73c922f92b3e40c9f089daa26ed1e9c4c1ccf
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-04-01 16:49:46 -0500

    Actually completed task's 1 and 2.

--- scripts.js
+++ scripts.js
@@ -22,43 +22,43 @@ $(document).ready(function() {
         $.each(response, function(index, item) {
           console.log(item);
           carouselHtml += `
-            <div class="carousel-item">
-              <div class="row mx-auto align-items-center">
-                <div class="">
-                  <img src="${item.pic_url}" class="d-block align-self-center" alt="Carousel Pic ${index + 1}" />
-                </div>
-                <div class="">
-                  <div class="quote-text">
-                    <p class="text-white text-wrap">${item.text}</p>
-                    <h4 class="text-white font-weight-bold">${item.name}</h4>
-                    <span class="text-white">${item.title}</span>
-                  </div>
+          <div class="slick-slide">
+            <div class="row mx-auto align-items-center">
+              <div class="col-12 col-sm-2 col-lg-2 offset-lg-1 text-center">
+                <img src="${item.pic_url}" class="d-block align-self-center" alt="Carousel Pic ${index + 1}" />
+              </div>
+              <div class="col-12 col-sm-7 offset-sm-2 col-lg-9 offset-lg-0">
+                <div class="quote-text">
+                  <p class="text-white">${item.text}</p>
+                  <h4 class="text-white font-weight-bold">${item.name}</h4>
+                  <span class="text-white">${item.title}</span>
                 </div>
               </div>
             </div>
-          `;
+          </div>
+        `;
         });
 
-        $(".carousel").html(carouselHtml);
-        $(".carousel-item:first").addClass("active");
+        $("#quotesCarousel").html(carouselHtml);
+        // $(".carousel-item:first").addClass("active");
 
-        $(".carousel").slick({
+        $(".slick-carousel-quotes").slick({
           arrows: false
         });
 
-        $(".carousel-control-prev, .carousel-control-next").click(function(e) {
+        $(".slick-prev-quotes, .slick-next-quotes").click(function(e) {
           e.preventDefault();
         });
 
-        $(".arrow-left").click(function() {
-          $(".carousel").slick("slickPrev");
+        $(".arrow-left-quotes").click(function() {
+          $(".slick-carousel-quotes").slick("slickPrev");
         });
 
-        $(".arrow-right").click(function() {
-          $(".carousel").slick("slickNext");
+        $(".arrow-right-quotes").click(function() {
+          $(".slick-carousel-quotes").slick("slickNext");
         });
 
-        $(".carousel").show();
+        $(".slick-carousel-quotes").show();
       },
       error: function() {
         hideLoader();
@@ -67,8 +67,6 @@ $(document).ready(function() {
     });
   }
   fetchCarouselData();
-
-
 
   // Popular Section
   function fetchPopularData() {
@@ -84,39 +82,64 @@ $(document).ready(function() {
 
         $.each(data, function(index, item) {
           carouselHtml += `
-          <div class="col-12 col-sm-6 col-md-6 col-lg-3 mb-4">
-            <div class="card position-relative style="padding: 12.5px;">
-              <div class="col-12">
-                <img src="${item.thumb_url}" class="card-img-top" alt="Video thumbnail">
-                <div class="play-overlay position-absolutetranslate-middle">
-                  <img src="images/play.png" alt="Play" width="64px" class="align-self-center">
+          <div class="col-12 col-sm-6 col-md-6 col-lg-3 d-flex justify-content-center justify-content-md-end justify-content-lg-center zindex-1">
+            <div class="card position-relative">
+              <img src="${item.thumb_url}" class="card-img-top" alt="Video thumbnail">
+              <div class="card-img-overlay text-center">
+                <img src="images/play.png" alt="Play" width="64px" class="align-self-center play-overlay" />
+              </div>
+              <div class="card-body">
+                <h5 class="card-title font-weight-bold">${item.title}</h5>
+                <p class="card-text text-muted">${item['sub-title']}</p>
+                <div class="creator d-flex align-items-center">
+                  <img src="${item.author_pic_url}" alt="Creator of Video" width="30px" class="rounded-circle">
+                  <h6 class="pl-3 m-0 main-color">${item.author}</h6>
                 </div>
-                <div class="card-body">
-                  <h5 class="card-title font-weight-bold">${item.title}</h5>
-                  <p class="card-text text-muted">${item['sub-title']}</p>
-                  <div class="creator d-flex align-items-center">
-                    <img src="${item.author_pic_url}" alt="Creator of Video" width="30px" class="rounded-circle">
-                    <h6 class="pl-3 m-0 main-color">${item.author}</h6>
+                <div class="info pt-3 d-flex justify-content-between">
+                  <div class="star-rating">
+                    ${generateStarRating(item.star)}
                   </div>
-                  <div class="info pt-3 d-flex justify-content-between">
-                    <div class="star-rating">
-                      ${generateStarRating(item.star)}
-                    </div>
-                    <span class="main-color">${item.duration}</span>
-                  </div>
+                  <span class="main-color">${item.duration}</span>
                 </div>
               </div>
             </div>
           </div>`;
         });
 
-        $("#popularCarousel .carousel-inner").html(carouselHtml);
-        $("#popularCarousel").slick({
+        $("#popularCarousel").html(carouselHtml);
+        $(".slick-carousel-popular").slick({
           arrows: false,
           slidesToShow: 4,
-          slidesToScroll: 1
+          slidesToScroll: 1,
+          responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2
+              }
+            },
+            {
+              breakpoint: 576,
+              settings: {
+                slidesToShow: 1
+              }
+            }
+          ]
         });
+        $(".arrow-left-popular, .arrow-right-popular").click(function(e) {
+          e.preventDefault();
+        });
+
+        $(".slick-prev-popular").click(function() {
+          $(".slick-carousel-popular").slick("slickPrev");
+        });
+
+        $(".slick-next-popular").click(function() {
+          $(".slick-carousel-popular").slick("slickNext");
+        });
+
       },
+
       error: function() {
         $(".loader").hide();
         console.log("Error getting API data");
@@ -135,6 +158,7 @@ $(document).ready(function() {
     starHtml += "</div>";
     return starHtml;
   };
+
 
   fetchPopularData();
 });
