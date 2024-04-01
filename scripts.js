7c97767cc43ddc3a81f02c2c8f8ca9dc58447d3f
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-04-01 00:10:10 -0500

    workin on task 1, lost arrows.

--- scripts.js
+++ scripts.js
@@ -1,69 +1,74 @@
-// JavaScript for Web App
-
 $(document).ready(function() {
-
-  // Quotes Section
   function showLoader() {
     $(".loader").show();
   }
+
   function hideLoader() {
     $(".loader").hide();
   }
+
   function fetchCarouselData() {
     $.ajax({
-      url: "https://smileschool-api.hbtn.info/quotes",
+      url:  "https://smileschool-api.hbtn.info/quotes",
       method: "GET",
       beforeSend: function() {
         showLoader();
       },
-      success: function(data) {
+      success: function(response) {
         hideLoader();
 
         var carouselHtml = "";
 
-        $.each(data, function(index, item) {
+        $.each(response, function(index, item) {
+          console.log(item);
           carouselHtml += `
-          <div class="carousel-item zindex-1">
-            <div class="row mx-auto align-items-center">
-              <div class="">
-                <img src="${item.pic_url}" class="d-block align-self-center" alt="Carousel Pic ${index + 1}" />
-              </div>
-              <div class="">
-                <div class="quote-text">
-                  <p class="text-white text-wrap">${item.text}</p>
-                  <h4 class="text-white font-weight-bold">${item.name}</h4>
-                  <span class="text-white">${item.title}</span>
+            <div class="carousel-item">
+              <div class="row mx-auto align-items-center">
+                <div class="">
+                  <img src="${item.pic_url}" class="d-block align-self-center" alt="Carousel Pic ${index + 1}" />
+                </div>
+                <div class="">
+                  <div class="quote-text">
+                    <p class="text-white text-wrap">${item.text}</p>
+                    <h4 class="text-white font-weight-bold">${item.name}</h4>
+                    <span class="text-white">${item.title}</span>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>`;
+          `;
         });
 
-        $("#quotesCarousel .carousel-inner").html(carouselHtml);
-        $("#carouselExampleControls").slick({
+        $(".carousel").html(carouselHtml);
+        $(".carousel-item:first").addClass("active");
+
+        $(".carousel").slick({
           arrows: false
         });
 
         $(".carousel-control-prev, .carousel-control-next").click(function(e) {
-            e.preventDefault();
+          e.preventDefault();
         });
 
         $(".arrow-left").click(function() {
-          $("#quotesCarousel").slick("slickPrev");
+          $(".carousel").slick("slickPrev");
         });
 
         $(".arrow-right").click(function() {
-          $("#quotesCarousel").slick("slickNext");
+          $(".carousel").slick("slickNext");
         });
 
-        $("#quotesCarousel").show();
+        $(".carousel").show();
       },
       error: function() {
         hideLoader();
-        console.log("Error getting API data");
+        console.log("error getting api");
       }
     });
   }
+  fetchCarouselData();
+
+
 
   // Popular Section
   function fetchPopularData() {
@@ -78,38 +83,26 @@ $(document).ready(function() {
         var carouselHtml = "";
 
         $.each(data, function(index, item) {
-          var columnClass = "col-12 col-sm-6 col-md-6 col-lg-3";
-          if (index === 0) {
-            columnClass += " d-flex justify-content-center justify-content-md-end justify-content-lg-center";
-          } else if (index === 1) {
-            columnClass += " d-none d-sm-flex justify-content-md-start justify-content-lg-center";
-          } else {
-            columnClass += " d-none d-lg-flex justify-content-center";
-          }
           carouselHtml += `
-          <div class="carousel-item active">
-            <div class="row align-items-center mx-auto">
-              <div class="col-12 col-md-6 col-lg-3 mb-4 d-flex justify-content-center justify-content-md-end justify-content-lg-center">
-                <div class="card position-relative style="padding: 12.5px;">
-                  <div class="col-12">
-                    <img src="${item.thumb_url}" class="card-img-top" alt="Video thumbnail">
-                    <div class="play-overlay position-absolutetranslate-middle">
-                        <img src="images/play.png" alt="Play" width="64px" class="align-self-center">
+          <div class="col-12 col-sm-6 col-md-6 col-lg-3 mb-4">
+            <div class="card position-relative style="padding: 12.5px;">
+              <div class="col-12">
+                <img src="${item.thumb_url}" class="card-img-top" alt="Video thumbnail">
+                <div class="play-overlay position-absolutetranslate-middle">
+                  <img src="images/play.png" alt="Play" width="64px" class="align-self-center">
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title font-weight-bold">${item.title}</h5>
+                  <p class="card-text text-muted">${item['sub-title']}</p>
+                  <div class="creator d-flex align-items-center">
+                    <img src="${item.author_pic_url}" alt="Creator of Video" width="30px" class="rounded-circle">
+                    <h6 class="pl-3 m-0 main-color">${item.author}</h6>
+                  </div>
+                  <div class="info pt-3 d-flex justify-content-between">
+                    <div class="star-rating">
+                      ${generateStarRating(item.star)}
                     </div>
-                    <div class="card-body">
-                        <h5 class="card-title font-weight-bold">${item.title}</h5>
-                        <p class="card-text text-muted">${item['sub-title']}</p>
-                      <div class="creator d-flex align-items-center">
-                          <img src="${item.author_pic_url}" alt="Creator of Video" width="30px" class="rounded-circle">
-                          <h6 class="pl-3 m-0 main-color">${item.author}</h6>
-                      </div>
-                      <div class="info pt-3 d-flex justify-content-between">
-                        <div class="star-rating">
-                            ${generateStarRating(item.star)}
-                        </div>
-                        <span class="main-color">${item.duration}</span>
-                      </div>
-                    </div>
+                    <span class="main-color">${item.duration}</span>
                   </div>
                 </div>
               </div>
@@ -117,22 +110,12 @@ $(document).ready(function() {
           </div>`;
         });
 
-        $("#popularCarousel").html(carouselHtml);
+        $("#popularCarousel .carousel-inner").html(carouselHtml);
         $("#popularCarousel").slick({
           arrows: false,
           slidesToShow: 4,
           slidesToScroll: 1
         });
-        $(".carousel-control-prev, .carousel-control-next").click(function(e) {
-          e.preventDefault();
-        });
-        $(".popular .arrow-left").click(function() {
-            $("#popularCarousel").slick("slickPrev");
-        });
-        $(".popular .arrow-right").click(function() {
-            $("#popularCarousel").slick("slickNext");
-        });
-        $(".carousel").show();
       },
       error: function() {
         $(".loader").hide();
@@ -153,6 +136,5 @@ $(document).ready(function() {
     return starHtml;
   };
 
-  fetchCarouselData();
   fetchPopularData();
 });
